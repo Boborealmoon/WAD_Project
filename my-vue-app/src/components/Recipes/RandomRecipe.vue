0e6b6f4a6ad26718cd8ca83e7a10b64ba8585772
lemuelfwl <lemuelfwl@gmail.com>
2023-10-05 15:22:52 +0800

    random recipe comp
    yuh

--- my-vue-app/src/components/Recipes/RandomRecipe.vue
+++ my-vue-app/src/components/Recipes/RandomRecipe.vue
@@ -2,13 +2,13 @@
 <template>
   <div>
     <button @click="getRandomRecipe">Click to get random recipe</button>
-    <div v-if="RandomRecipe" :style="{display: 'flex'}">
-        <div >
-            <h1>Title: {{ Title }}</h1>
-            <h2>DishTypes: {{ DishTypes }} </h2>
-            <h2 v-if="Diets">Diets: {{ Diets }} </h2>
-            <h2>Prep Time: {{ PrepTime }}</h2>
-            <div>Cooking Steps: 
+    <div class="rando-recipe" v-if="RandomRecipe"> 
+        <!-- <div> -->
+            <!-- <h1>Title: {{ Title }}</h1> -->
+            <!-- <h2>DishTypes: {{ DishTypes }} </h2> -->
+            <!-- <h2 v-if="Diets">Diets: {{ Diets }} </h2> -->
+            <!-- <h2>Prep Time: {{ PrepTime }}</h2> -->
+            <!-- <div>Cooking Steps: 
                 <ul v-if="CookingSteps">
                     <div v-for="instructions in CookingSteps" :key="instructions">
                         <li v-for="(step,index) in instructions.steps" :key="step">
@@ -24,12 +24,18 @@
                         </li>
                     </div>
                 </ul>
-            </div>
-            <h3>Summary:</h3>
-            <p v-html="Summary"></p>
+            </div> -->
+            <!-- <h3>Summary:</h3>
+            <p v-html="Summary"></p> -->
+        <!-- </div> -->
+        <div class="rando-recipe-header">
+          Recipe of the Day
         </div>
-        <div >
-            <img :src="imgUrl">
+        <div class="rando-recipe-img">
+            <img class="rando-img" :src="imgUrl">
+        </div>
+        <div class="rando-recipe-info">
+          <h1>{{ Title }}</h1>
         </div>
     </div>
   </div>
@@ -66,4 +72,57 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+ .rando-recipe{
+  /* width: 994px;
+  height: 363px; */
+  background: #AED4B7;
+  border-radius: 40px;
+  width: 70vw;
+  height: 30vh;
+ }
+
+ .rando-recipe-header{
+  height: auto;
+  width: auto;
+  color: white;
+  font-size: 24px;
+  font-family: Inter;
+  font-weight: 600;
+  word-wrap: break-word;
+  margin-left: 4%;
+ }
+
+ .rando-recipe-img{
+  width: 90%;
+  height: 70%;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 20px;
+  margin-left: 4%;
+  src: "https://via.placeholder.com/905x226";
+ }
+
+ .rando-img{
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center;
+  width: 100%;
+  height: 100%;
+  border-radius: 20px;
+
+ }
+
+ .rando-recipe-info{
+  height: auto;
+  width: auto;
+  color: white;
+  font-size: 36px;
+  font-family: Inika;
+  font-weight: 400;
+  word-wrap: break-word;
+  margin-left: 4%;
+  margin-top: 1%;
+ }
+
+</style>
+
