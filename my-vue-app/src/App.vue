d0b9c6222959e4f586222da5aa7df9f56490906d
lemuelfwl <lemuelfwl@gmail.com>
2023-10-06 13:51:35 +0800

    recipeofday updated

--- my-vue-app/src/App.vue
+++ my-vue-app/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>Hello World</h1>
-    <button @click="fetchProductsByPromo">Fetch Supermarket Data</button>
+    <button @click="fetchProductsByPrice">Fetch Supermarket Data</button>
       <!-- Display supermarket data here -->
       <div :style="{display: 'flex'}" v-for='product in supermarketData' :key="product.product_link">
         <div :style="{ width: '50%'} ">
@@ -15,55 +15,49 @@
         </div>
       </div>
     <br>
-    <RandomRecipe/>
+    <RecipeOfDay/>
     <RecipesByIngredients/>
   </div>
 </template>
 
 <script>
+import RecipeOfDay from './components/Recipes/RecipeOfDay.vue';
+
 export default {
-  name: 'YourComponentName',
-  data() {
-    return {
-      title: 'eggs', // this value should be '' then v-model to input text 
-      supermarketData: [],
-      supermarketName: 'FairPrice', // Other options include: ColdStorage, FairPrice 
-      price: '5', // this value should be 0 by default (?) and then v-model to input text 
-      ascending: true // default value is true, sort in increasing price
-    };
-  },
-  methods: {
-    async fetchAllProducts(){
-      this.supermarketData = await this.$smAPI.fetchAllProducts(); 
+    name: 'YourComponentName',
+    data() {
+        return {
+            title: 'eggs',
+            supermarketData: [],
+            supermarketName: 'FairPrice',
+            price: '5',
+            ascending: true // default value is true, sort in increasing price
+        };
     },
-    async fetchProductsBySupermarket(){
-      this.supermarketData = await this.$smAPI.fetchProductsBySupermarket(this.supermarketName, false);
+    methods: {
+        async fetchAllProducts() {
+            this.supermarketData = await this.$smAPI.fetchAllProducts();
+        },
+        async fetchProductsBySupermarket() {
+            this.supermarketData = await this.$smAPI.fetchProductsBySupermarket(this.supermarketName, false);
+        },
+        async fetchProductsByTitle() {
+            this.supermarketData = await this.$smAPI.fetchProductsByTitle(this.title, true);
+        },
+        async fetchProductsByPrice() {
+            this.supermarketData = await this.$smAPI.fetchProductsByPrice(this.price, false);
+        },
+        async fetchProductsByTitleAndPrice() {
+            this.supermarketData = await this.$smAPI.fetchProductsByTitleAndPrice(this.title, this.price);
+        },
+        async fetchProductsByPromo() {
+            this.supermarketData = await this.$smAPI.fetchProductsByPromo(false);
+        },
+        async fetchProductsByPromoAndSupermarket() {
+            this.supermarketData = await this.$smAPI.fetchProductsByPromoAndSupermarket(this.supermarketName, false);
+        },
     },
-    async fetchProductsByTitle(){
-      this.supermarketData = await this.$smAPI.fetchProductsByTitle(this.title, true);
-    },
-    async fetchProductsByPrice(){
-      this.supermarketData = await this.$smAPI.fetchProductsByPrice(this.price, false);
-    },
-    async fetchProductsByTitleAndPrice(){
-      this.supermarketData = await this.$smAPI.fetchProductsByTitleAndPrice(this.title, this.price);
-    },
-    async fetchProductsByPromo(){
-      this.supermarketData = await this.$smAPI.fetchProductsByPromo(false);
-    },
-    async fetchProductsByPromoAndSupermarket(){
-      this.supermarketData = await this.$smAPI.fetchProductsByPromoAndSupermarket(this.supermarketName, false);
-    },
-  },
-  // You can use the `onMounted` hook to fetch data when the component is mounted
-  // async mounted() {
-  //   try {
-  //     this.supermarketData = await this.$smAPI.fetchAllProducts(); // Call the function you need
-  //     // You can process or display the data here
-  //   } catch (error) {
-  //     console.error('Error fetching supermarket data:', error);
-  //   }
-  // },
+    components: { RecipeOfDay }
 };
 </script>
 
---
+++ my-vue-app/src/components/Recipes/RecipeOfDay.vue
@@ -0,0 +1,99 @@
+<!-- This component generates the Recipe of the Day-->
+<template>
+  <div>
+    <button @click="getRandomRecipe">Click to get random recipe</button>
+    <div class="container" v-if="RandomRecipe">
+      <div class="row justify-content-start">
+        <div class="rando-recipe col-lg-10 col-md-11 col-sm-12">
+            <div class="rando-recipe-header">
+            Recipe of the Day
+            </div>
+      <div class="rando-recipe-img">
+        <img class="rando-img img-fluid" :src="imgUrl" alt="Recipe Image">
+      </div>
+      <div class="rando-recipe-info">
+        <h1>{{ Title }}</h1>
+      </div>
+    </div>
+  </div>
+</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      RandomRecipe: null,
+      Title: '',
+      DishTypes: [],
+      Diets: [],
+      PrepTime: '',
+      imgUrl: '',
+      CookingSteps: [],
+      Summary: '',
+    };
+  },
+  methods: {
+    async getRandomRecipe() {
+      let RandomRecipeObj = await this.$spoonAPI.getRandomRecipe(); 
+      this.RandomRecipe = RandomRecipeObj.recipes[0]
+      this.Title = this.RandomRecipe.title;
+      this.DishTypes = this.RandomRecipe.dishTypes;
+      this.Diets = this.RandomRecipe.diets;
+      this.PrepTime = this.RandomRecipe.readyInMinutes;
+      this.imgUrl = this.RandomRecipe.image;
+      this.CookingSteps = this.RandomRecipe.analyzedInstructions;
+      this.Summary = this.RandomRecipe.instructions;
+      console.log(this.CookingSteps);
+    },
+  },
+};
+</script>
+
+
+<style scoped>
+
+ .container{
+  margin-left: 0!important;
+  margin-right: 0!important;
+ }
+ .rando-recipe{
+  background: #AED4B7;
+  border-radius: 40px;
+  padding: 20px;
+ }
+
+ .rando-recipe-header{
+  color: white;
+  font-size: 1.5em; 
+  font-family: Inter;
+  font-weight: 600;
+  word-wrap: break-word;
+  margin-bottom: 10px;
+ }
+
+ .rando-recipe-img{
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 20px;
+  margin-bottom: 10px;
+  overflow: hidden;
+ }
+
+ .rando-img{
+  border-radius: 20px;
+  object-fit: contain;
+  height: 100%;
+  width: 100%;
+ }
+
+ .rando-recipe-info{
+  color: white;
+  font-size: 2.25em;
+  font-family: Inika;
+  font-weight: 400;
+  word-wrap: break-word;
+ }
+
+
+</style>
