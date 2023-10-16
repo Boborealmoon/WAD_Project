18b0a12e28f67f6e3e4f03446512a0785be4712d
Zavier <zavier.yan.2022@scis.smu.edu.sg>
2023-10-16 13:06:39 +0800

    Update App.vue

--- my-vue-app/src/App.vue
+++ my-vue-app/src/App.vue
@@ -16,56 +16,49 @@
           <a href="#" class="btn btn-primary">Go somewhere</a>
         </div>
       </div>
-    <br>
-    <RandomRecipe/>
-    <RecipesByIngredients/>
+    </div>
+    <br /> -->
+    <router-view>
+      
+    </router-view>
+
   </div>
 </template>
 
 <script>
+
 export default {
-  name: 'YourComponentName',
   data() {
     return {
-      title: 'eggs', // this value should be '' then v-model to input text 
       supermarketData: [],
-      supermarketName: 'FairPrice', // Other options include: ColdStorage, FairPrice 
-      price: '5', // this value should be 0 by default (?) and then v-model to input text 
-      ascending: true // default value is true, sort in increasing price
+      ascending: true, // default value is true, sort in increasing price
     };
   },
   methods: {
-    async fetchAllProducts(){
-      this.supermarketData = await this.$smAPI.fetchAllProducts(); 
+    async fetchProductsByPromo() {
+        this.supermarketData = await this.$smAPI.fetchProductsByPromo(false);
+        this.initOwlCarousel();
     },
-    async fetchProductsBySupermarket(){
-      this.supermarketData = await this.$smAPI.fetchProductsBySupermarket(this.supermarketName, false);
+
+    initOwlCarousel() {
+        $(document).ready(function(){
+          $(".owl-carousel").owlCarousel(
+            {
+              items: 3
+            }
+          );
+        });
     },
-    async fetchProductsByTitle(){
-      this.supermarketData = await this.$smAPI.fetchProductsByTitle(this.title, true);
-    },
-    async fetchProductsByPrice(){
-      this.supermarketData = await this.$smAPI.fetchProductsByPrice(this.price, false);
-    },
-    async fetchProductsByTitleAndPrice(){
-      this.supermarketData = await this.$smAPI.fetchProductsByTitleAndPrice(this.title, this.price);
-    },
-    async fetchProductsByPromo(){
-      this.supermarketData = await this.$smAPI.fetchProductsByPromo(false);
-    },
-    async fetchProductsByPromoAndSupermarket(){
-      this.supermarketData = await this.$smAPI.fetchProductsByPromoAndSupermarket(this.supermarketName, false);
+
+    async fetchProductsByPromoAndSupermarket() {
+      this.supermarketData =
+        await this.$smAPI.fetchProductsByPromoAndSupermarket(
+          this.supermarketName,
+          false
+        );
     },
   },
-  // You can use the `onMounted` hook to fetch data when the component is mounted
-  // async mounted() {
-  //   try {
-  //     this.supermarketData = await this.$smAPI.fetchAllProducts(); // Call the function you need
-  //     // You can process or display the data here
-  //   } catch (error) {
-  //     console.error('Error fetching supermarket data:', error);
-  //   }
-  // },
+
 };
 
 </script>
